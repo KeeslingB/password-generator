28bef119df275bd5717325a9a61e91cc0404b4d0
unknown <Breke64@gmail.com>
2023-10-08 13:07:54 -0400

    worked on homework js code -still having trouble validating pw length and randomizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,75 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];  
-var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]; 
-var numbers = ["1","2","3","4","5","6","7","8","9"]; 
-var specialCharacters = ["!","@","#","$","%","^","&","*"];
-var finalPass =[""]; 
-var bigB = [specialCharacters + upperCaseLetters +lowerCaseLetters + numbers +specialCharacters] ;
-length = password.length;
+
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+var finalPass = [""];
+
+var numberOfCharacthers = [""];
 
 // var numberOfCharacthers = prompt("How many Characters?");  
 // var lettersPrompt = confirm("Would you like upper case letters?");
-// var finalnumbers = confirm("Would you like numbers?");
+// var numberPrompt = confirm("Would you like numbers?");
 // var lowerCase = confirm("Would you like special charecters?");
 
 
-function generatePassword(){
-  passLength();
-  upperPrompts();
-  numbPrompt();
-  lowerPrompt();
-  passLength();
-  // randomFinal();
-return finalPass;
+
+function generatePassword() {
+ passwordLength();
+ lowerCase();
+ upperCasePrompt();
+ numbersInc();
+ specialC();
+ for(var i = 0; i < finalPass.length; i++){
+  // finalPass = Math.floor(Math.random() * passwordLength.length);
+  console.log(finalPass);
+ }return finalPass;
 }
 
-function passLength() {        // between 8-128
-  lengthP = prompt('How many Charecters would you like?')
-  if (lengthP < 8 || lengthP > 128) {
-    alert('Your password must be between 8 and 128 charecters in length.')
-  }return finalPass;
+
+
+
+
+function passwordLength(numberOfCharacthers) {
+  numberOfCharecters = prompt('How many charecters would you like included?');
+  if (numberOfCharecters < 8 || numberOfCharecters > 128) {
+    alert('Password must be between 8-128 charecters!');
+  }return;
 }
 
-function upperPrompts() {
-  var lettersPrompt = confirm("Would you like upper case letters?");
-  if(lettersPrompt){
-    finalPass = ([finalPass + upperCaseLetters]);
-  }
+function lowerCase () {
+  lowerPrompt = confirm('Would you like Lowercase letters included?');
+  if(lowerPrompt) {
+    finalPass = (lowerCaseLetters + finalPass);
+  }return;
 }
 
-function numbPrompt(){
-  var finalnumbers = confirm("Would you like numbers?");
-  if(finalnumbers){
-    finalPass = ([finalPass + numbers]);
-  }
+function upperCasePrompt() {
+   lettersPrompt = confirm('Would you like upper case letters included?')
+  if (lettersPrompt) {
+    finalPass = (upperCaseLetters + finalPass);
+  }return;
 }
 
-function lowerPrompt() {
-  var lowerCase = confirm("Would you like special charecters?");
-  if(lowerCase){
-    finalPass = ([finalPass + specialCharacters]);
-  }
+function numbersInc() {
+  numbersP = confirm('Would you like numbers included?');
+  if (numbersP) {
+    finalPass = (numbers + finalPass);
+  }return;
 }
 
-// function randomFinal(){
-// for (let i = 0; i < length; i++) {
-//   let finalPass = [Math.floor(Math.random() * finalPass.length)];
-// }return finalPass;
-// }
+function specialC() { 
+   special = confirm('Would you like special charecters included?');
+  if (special) {
+    finalPass = (specialCharacters + finalPass);
+  }return;
+}
 
 
 // Write password to the #password input
@@ -69,6 +82,17 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
+generateBtn.addEventListener("click", writePassword); {
 
 }
+
+
+
+// function passLength() {        // between 8-128
+//   lengthP = prompt('How many Charecters would you like?')
+//   if (lengthP < 8 || lengthP > 128) {
+//     alert('Your password must be between 8 and 128 charecters in length.')
+//   }return finalPass;
+//
+
+
